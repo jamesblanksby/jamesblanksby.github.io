49871037863ab5eb2152435b90e34f0f39c862c0
James Blanksby <james@thisismyengine.com>
2023-07-24 15:43:35 +0300

    Removed header hover effect

--- src/script/site.js
+++ src/script/site.js
@@ -9,69 +9,8 @@ $(init);
 
 /* --------------------------------------------------------------------- INIT --- */
 function init() {
-    // header
-    header_init();
-
     // footer
     footer_init();
-}
-
-
-/* ////////////////////////////////////////////////////////////////////////////// */
-/* /////////////////////////////////////////////////////////////////// HEADER /// */
-/* ////////////////////////////////////////////////////////////////////////////// */
-
-/* --------------------------------------------------------------------- INIT --- */
-function header_init() {
-    // event
-    header_event();
-}
-
-/* -------------------------------------------------------------------- EVENT --- */
-function header_event() {
-    // event hover
-    header_event_hover();
-}
-
-/* ------------------------------------------------------------ EVENT : HOVER --- */
-function header_event_hover() {
-    var $document,
-        $heading;
-    var original = {},
-        scramble = {},
-        interval;
-
-    // cache elements
-    $document = $(document);
-    $heading = $('header h1');
-
-    // listen for mouseover
-    $heading.on('mouseover', function() {
-        // original text
-        original.document = $document.prop('title');
-        original.heading = $heading.text();
-
-        // scramble interval
-        interval = setInterval(function() {
-            // scramble text
-            scramble.document = text_scramble(original.document);
-            scramble.heading = text_scramble(original.heading);
-
-            // text content
-            $document.prop('title', scramble.document);
-            $heading.text(scramble.heading);
-        }, 50);
-    });
-
-    // listen for mouseout
-    $heading.on('mouseout', function() {
-        // destroy interval
-        clearInterval(interval);
-
-        // text content
-        $document.prop('title', original.document);
-        $heading.text(original.heading);
-    });
 }
 
 
@@ -127,45 +66,4 @@ function header_code() {
 
     // uuid content
     $code.text(uuid);
-}
-
-
-/* ////////////////////////////////////////////////////////////////////////////// */
-/* ///////////////////////////////////////////////////////////////////// TEXT /// */
-/* ////////////////////////////////////////////////////////////////////////////// */
-
-/* ----------------------------------------------------------------- SCRAMBLE --- */
-function text_scramble(text) {
-    var symbol;
-
-    // symbol options
-    symbol = '!<>-_\\/[]{}—=+*^?#'.split('');
-
-    // scramble text
-    text = text.split('').map(function(a) { return random(4) > 1 ? random(symbol) : a; }).join('');
-
-    return text;
-}
-
-
-/* ////////////////////////////////////////////////////////////////////////////// */
-/* /////////////////////////////////////////////////////////////////// RANDOM /// */
-/* ////////////////////////////////////////////////////////////////////////////// */
-
-/* ------------------------------------------------------------------- RANDOM --- */
-function random(input) {
-    var value;
-
-    // validate number
-    if (typeof input === 'number') {
-        // random integer
-        value = Math.floor((Math.random() * input));
-    }
-    // validate array
-    else if (Array.isArray(input)) {
-        // random array element
-        value = input[random(input.length)];
-    }
-
-    return value;
 }
